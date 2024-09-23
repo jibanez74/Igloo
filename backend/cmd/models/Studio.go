4c83f5bed4044c0f12bd7385375490a10d7544f4
Jose Ibanez <jose.ibanez@blindpenguincoder.com>
2024-09-23 17:55:28 -0400

    fixed relation between movies and studios

--- backend/cmd/models/Studio.go
+++ backend/cmd/models/Studio.go
@@ -11,7 +11,7 @@ type Studio struct {
 	Name    string   `gorm:"not null;index" json:"name"`
 	Country string   `gorm:"not null;default:'unknown'" json:"country"`
 	Logo    string   `gorm:"not null;default:'unknown'" json:"logo"`
-	Movies  []*Movie `gorm:"many2many;movie_genres" json:"movies"`
+	Movies  []*Movie `gorm:"many2many;movie_studios" json:"movies"`
 }
 
 func (s *Studio) BeforeSave(tx *gorm.DB) error {
