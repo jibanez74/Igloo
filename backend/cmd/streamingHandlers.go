4075b3d9cc790da3d41cfaa06022b2f96a00917a
Jose Ibanez <jose.ibanez@blindpenguincoder.com>
2024-10-02 14:49:33 -0400

    added handler to transcode video using ffmpeg

--- backend/cmd/streamingHandlers.go
+++ backend/cmd/streamingHandlers.go
@@ -4,12 +4,16 @@ import (
 	"errors"
 	"fmt"
 	"igloo/cmd/helpers"
+	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
 )
+
+const ffmpegPath = "/bin/ffmpeg"
 
 func (app *config) DirectStreamVideo(w http.ResponseWriter, r *http.Request) {
 	filePath := r.URL.Query().Get("filePath")
@@ -89,5 +93,103 @@ func (app *config) DirectStreamVideo(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 
 		http.ServeContent(w, r, filePath, time.Now(), file)
+	}
+}
+
+func (app *config) StreamTranscodedVideo(w http.ResponseWriter, r *http.Request) {
+	var cmdArgs []string
+
+	filePath := r.URL.Query().Get("filePath")
+	if filePath == "" {
+		helpers.ErrorJSON(w, errors.New("file path is required"), http.StatusBadRequest)
+		return
+	}
+
+	_, err := os.Stat(filePath)
+	if err != nil || os.IsNotExist(err) {
+		helpers.ErrorJSON(w, errors.New("file not found"), http.StatusNotFound)
+		return
+	}
+	cmdArgs = append(cmdArgs, "-i", filePath)
+
+	videoCodec := r.URL.Query().Get("videoCodec")
+	if videoCodec == "" {
+		videoCodec = "libx264"
+	}
+	cmdArgs = append(cmdArgs, "-c:v", videoCodec)
+
+	videoHeight := r.URL.Query().Get("videoHeight")
+	if videoHeight == "" {
+		helpers.ErrorJSON(w, errors.New("video height is required"), http.StatusBadRequest)
+		return
+	}
+	cmdArgs = append(cmdArgs, "-vf", fmt.Sprintf("scale=-1:%s", videoHeight))
+
+	videoBitRate := r.URL.Query().Get("videoBitRate")
+	if videoBitRate == "" {
+		videoBitRate = "2000k"
+	}
+	cmdArgs = append(cmdArgs, "-b:v", videoBitRate)
+
+	audioCodec := r.URL.Query().Get("audioCodec")
+	if audioCodec == "" {
+		audioCodec = "aac"
+	}
+	cmdArgs = append(cmdArgs, "-c:a", audioCodec)
+
+	audioChannels := r.URL.Query().Get("audioChannels")
+	if audioChannels == "" {
+		audioChannels = "2"
+	}
+	cmdArgs = append(cmdArgs, "-ac", audioChannels)
+
+	audioBitRate := r.URL.Query().Get("audioBitRate")
+	if audioBitRate == "" {
+		audioBitRate = "128kk"
+	}
+	cmdArgs = append(cmdArgs, "-b:a", audioBitRate)
+
+	preset := r.URL.Query().Get("preset")
+	if preset == "" {
+		preset = "ultrafast"
+	}
+	cmdArgs = append(cmdArgs, "-preset", preset)
+
+	container := r.URL.Query().Get("container")
+	if container == "" {
+		container = "mp4"
+	}
+	cmdArgs = append(cmdArgs, "-f", container)
+
+	if container == "mp4" {
+		cmdArgs = append(cmdArgs, "-movflags", "frag_keyframe+empty_moov")
+	}
+
+	cmdArgs = append(cmdArgs, "pipe:1")
+
+	cmd := exec.Command(ffmpegPath, cmdArgs...)
+
+	cmdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		helpers.ErrorJSON(w, err)
+		return
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		helpers.ErrorJSON(w, err)
+		return
+	}
+
+	_, err = io.Copy(w, cmdOut)
+	if err != nil {
+		helpers.ErrorJSON(w, err)
+		return
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		helpers.ErrorJSON(w, err)
+		return
 	}
 }
