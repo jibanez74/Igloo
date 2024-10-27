ca4950be10366f9fb0c97106c91fcdc40b605384
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2024-10-27 18:22:19 -0400

    working on direct play

--- backend/cmd/streamingHandlers.go
+++ backend/cmd/streamingHandlers.go
@@ -1,110 +1,105 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"igloo/cmd/database/models"
-	"os"
-	"strconv"
-	"strings"
+  "errors"
+  "fmt"
+  "igloo/cmd/database/models"
+  "log"
+  "os"
+  "strconv"
+  "strings"
 
-	"github.com/gofiber/fiber/v2"
+  "github.com/gofiber/fiber/v2"
 )
 
 func (app *config) DirectStreamVideo(c *fiber.Ctx) error {
-	movieID, err := strconv.ParseUint(c.Params("id"), 10, 64)
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": err.Error(),
-		})
-	}
+  movieID, err := strconv.ParseUint(c.Params("id"), 10, 64)
+  if err != nil {
+    return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+      "error": err.Error(),
+    })
+  }
 
-	var movie models.Movie
-	movie.ID = uint(movieID)
+  var movie models.Movie
+  movie.ID = uint(movieID)
 
-	status, err := app.repo.GetMovieByID(&movie)
-	if err != nil {
-		return c.Status(status).JSON(fiber.Map{
-			"error": err.Error(),
-		})
-	}
+  status, err := app.repo.GetMovieByID(&movie)
+  if err != nil {
+    return c.Status(status).JSON(fiber.Map{
+      "error": err.Error(),
+    })
+  }
 
-	_, err = os.Stat(movie.FilePath)
-	if err != nil || os.IsNotExist(err) {
-		c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+  _, err = os.Stat(movie.FilePath)
+  if err != nil || os.IsNotExist(err) {
+    log.Println(err)
+    return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+      "error": err.Error(),
+    })
+  }
 
-		return err
-	}
+  rangeHeader := c.Get("Range")
 
-	rangeHeader := c.Get("Range")
+  c.Set("Content-Type", movie.ContentType)
 
-	c.Set("Content-Type", movie.ContentType)
+  size := int64(movie.Size)
 
-	size := int64(movie.Size)
+  var start int64 = 0
+  var end int64 = size - 1
 
-	var start int64 = 0
-	var end int64 = size - 1
+  if rangeHeader != "" {
+    file, err := os.Open(movie.FilePath)
+    if err != nil {
+      log.Println(err)
+      return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+        "error": err.Error(),
+      })
+    }
+    defer file.Close()
 
-	if rangeHeader != "" {
-		file, err := os.Open(movie.FilePath)
-		if err != nil {
-			c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": err.Error(),
-			})
+    rangeHeader = strings.Replace(rangeHeader, "bytes=", "", 1)
+    parts := strings.Split(rangeHeader, "-")
 
-			return err
-		}
-		defer file.Close()
+    if parts[0] != "" {
+      start, err = strconv.ParseInt(parts[0], 10, 64)
+      if err != nil || start < 0 || start >= size {
+        start = 0
+      }
+    }
 
-		rangeHeader = strings.Replace(rangeHeader, "bytes=", "", 1)
-		parts := strings.Split(rangeHeader, "-")
+    if parts[1] != "" {
+      end, err = strconv.ParseInt(parts[1], 10, 64)
+      if err != nil || end >= size {
+        end = size - 1
+      }
+    }
 
-		if parts[0] != "" {
-			start, err = strconv.ParseInt(parts[0], 10, 64)
-			if err != nil || start < 0 || start >= size {
-				start = 0
-			}
-		}
+    if start > end {
+      log.Println("invalid range")
+      return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+        "error": "invalid range",
+      })
+    }
 
-		if parts[1] != "" {
-			end, err = strconv.ParseInt(parts[1], 10, 64)
-			if err != nil || end >= size {
-				end = size - 1
-			}
-		}
+    contentLength := end - start + 1
 
-		if start > end {
-			err = errors.New("invalid range")
+    c.Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, size))
+    c.Set("Accept-Ranges", "bytes")
+    c.Set("Content-Length", fmt.Sprintf("%d", contentLength))
+    c.Status(fiber.StatusPartialContent)
 
-			c.Status(fiber.StatusRequestedRangeNotSatisfiable).JSON(fiber.Map{
-				"error": err.Error(),
-			})
+    _, err = file.Seek(start, 0)
+    if err != nil {
+      log.Println(err)
+      return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+        "error": err.Error(),
+      })
+    }
 
-			return err
-		}
+    return c.SendStream(file)
+  }
 
-		contentLength := end - start + 1
+  c.Set("Content-Length", fmt.Sprintf("%d", size))
 
-		c.Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, size))
-		c.Set("Accept-Ranges", "bytes")
-		c.Set("Content-Length", fmt.Sprintf("%d", contentLength))
-		c.Status(fiber.StatusPartialContent)
-
-		_, err = file.Seek(start, 0)
-		if err != nil {
-			c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-
-			return err
-		}
-
-		return c.SendStream(file)
-	}
-
-	c.Set("Content-Length", fmt.Sprintf("%d", size))
-
-	return c.SendFile(movie.FilePath)
+  return c.SendFile(movie.FilePath)
 }
