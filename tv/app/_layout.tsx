0c03d3b6b880f1c3281002ab9bde157c1acb90c9
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2024-11-16 16:15:20 -0400

    added nativewind for better styles workflow

--- tv/app/_layout.tsx
+++ tv/app/_layout.tsx
@@ -3,6 +3,7 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@/assets/global.css";
 
 const queryClient = new QueryClient();
 
