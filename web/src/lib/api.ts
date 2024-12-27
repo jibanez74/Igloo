7cc735d45e63c6284dc7a18474d5728f4f55f3fc
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2024-12-27 12:36:54 -0300

    working on adding csrf to backend

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -3,6 +3,25 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "/api/v1",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-CSRF-Token":
+      document
+        .querySelector('meta[name="csrf-token"]')
+        ?.getAttribute("content") || "",
+  },
 });
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(err);
+  }
+);
 
 export default api;
