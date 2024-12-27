b907fb967f16661e8da331bf52a2cf9d60c7f9a2
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2024-12-27 12:40:59 -0300

    removed api config for error responses

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -13,15 +13,4 @@ const api = axios.create({
   },
 });
 
-api.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response?.status === 401) {
-      window.location.href = "/login";
-    }
-
-    return Promise.reject(err);
-  }
-);
-
 export default api;
