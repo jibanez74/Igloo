f2052b85fd319090e252bd2295f27e974540f0c5
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2025-02-13 16:36:49 -0400

    working on fixing movie details query

--- backend/queries/movies.sql
+++ backend/queries/movies.sql
@@ -56,13 +56,16 @@ movie_studios AS (
 ),
 movie_cast AS (
     SELECT COALESCE(
-        json_agg(DISTINCT jsonb_build_object(
-            'id', a.id,
-            'name', a.name,
-            'thumb', a.thumb,
-            'character', cl.character,
-            'sort_order', cl.sort_order
-        ) ORDER BY cl.sort_order) FILTER (WHERE a.id IS NOT NULL), '[]'
+        json_agg(
+            jsonb_build_object(
+                'id', a.id,
+                'name', a.name,
+                'thumb', a.thumb,
+                'character', cl.character,
+                'sort_order', cl.sort_order
+            )
+            ORDER BY cl.sort_order
+        ) FILTER (WHERE a.id IS NOT NULL), '[]'
     ) as cast
     FROM movie_base m
     LEFT JOIN cast_list cl ON cl.movie_id = m.id
@@ -70,13 +73,15 @@ movie_cast AS (
 ),
 movie_crew AS (
     SELECT COALESCE(
-        json_agg(DISTINCT jsonb_build_object(
-            'id', a.id,
-            'name', a.name,
-            'thumb', a.thumb,
-            'job', cw.job,
-            'department', cw.department
-        )) FILTER (WHERE a.id IS NOT NULL), '[]'
+        json_agg(
+            jsonb_build_object(
+                'id', a.id,
+                'name', a.name,
+                'thumb', a.thumb,
+                'job', cw.job,
+                'department', cw.department
+            )
+        ) FILTER (WHERE a.id IS NOT NULL), '[]'
     ) as crew
     FROM movie_base m
     LEFT JOIN crew_list cw ON cw.movie_id = m.id
@@ -84,12 +89,14 @@ movie_crew AS (
 ),
 movie_extras AS (
     SELECT COALESCE(
-        json_agg(DISTINCT jsonb_build_object(
-            'id', me.id,
-            'title', me.title,
-            'url', me.url,
-            'kind', me.kind
-        )) FILTER (WHERE me.id IS NOT NULL), '[]'
+        json_agg(
+            jsonb_build_object(
+                'id', me.id,
+                'title', me.title,
+                'url', me.url,
+                'kind', me.kind
+            )
+        ) FILTER (WHERE me.id IS NOT NULL), '[]'
     ) as extras
     FROM movie_base m
     LEFT JOIN movie_extras me ON me.movie_id = m.id
@@ -97,47 +104,53 @@ movie_extras AS (
 movie_streams AS (
     SELECT 
         COALESCE(
-            json_agg(DISTINCT jsonb_build_object(
-                'id', vs.id,
-                'title', vs.title,
-                'index', vs.index,
-                'profile', vs.profile,
-                'aspect_ratio', vs.aspect_ratio,
-                'bit_rate', vs.bit_rate,
-                'bit_depth', vs.bit_depth,
-                'codec', vs.codec,
-                'width', vs.width,
-                'height', vs.height,
-                'coded_width', vs.coded_width,
-                'coded_height', vs.coded_height,
-                'color_transfer', vs.color_transfer,
-                'color_primaries', vs.color_primaries,
-                'color_space', vs.color_space,
-                'color_range', vs.color_range,
-                'frame_rate', vs.frame_rate,
-                'avg_frame_rate', vs.avg_frame_rate,
-                'level', vs.level
-            )) FILTER (WHERE vs.id IS NOT NULL), '[]'
+            json_agg(
+                jsonb_build_object(
+                    'id', vs.id,
+                    'title', vs.title,
+                    'index', vs.index,
+                    'profile', vs.profile,
+                    'aspect_ratio', vs.aspect_ratio,
+                    'bit_rate', vs.bit_rate,
+                    'bit_depth', vs.bit_depth,
+                    'codec', vs.codec,
+                    'width', vs.width,
+                    'height', vs.height,
+                    'coded_width', vs.coded_width,
+                    'coded_height', vs.coded_height,
+                    'color_transfer', vs.color_transfer,
+                    'color_primaries', vs.color_primaries,
+                    'color_space', vs.color_space,
+                    'color_range', vs.color_range,
+                    'frame_rate', vs.frame_rate,
+                    'avg_frame_rate', vs.avg_frame_rate,
+                    'level', vs.level
+                )
+            ) FILTER (WHERE vs.id IS NOT NULL), '[]'
         ) as video_streams,
         COALESCE(
-            json_agg(DISTINCT jsonb_build_object(
-                'id', aus.id,
-                'title', aus.title,
-                'index', aus.index,
-                'codec', aus.codec,
-                'channels', aus.channels,
-                'channel_layout', aus.channel_layout,
-                'language', aus.language
-            )) FILTER (WHERE aus.id IS NOT NULL), '[]'
+            json_agg(
+                jsonb_build_object(
+                    'id', aus.id,
+                    'title', aus.title,
+                    'index', aus.index,
+                    'codec', aus.codec,
+                    'channels', aus.channels,
+                    'channel_layout', aus.channel_layout,
+                    'language', aus.language
+                )
+            ) FILTER (WHERE aus.id IS NOT NULL), '[]'
         ) as audio_streams,
         COALESCE(
-            json_agg(DISTINCT jsonb_build_object(
-                'id', sub.id,
-                'title', sub.title,
-                'index', sub.index,
-                'codec', sub.codec,
-                'language', sub.language
-            )) FILTER (WHERE sub.id IS NOT NULL), '[]'
+            json_agg(
+                jsonb_build_object(
+                    'id', sub.id,
+                    'title', sub.title,
+                    'index', sub.index,
+                    'codec', sub.codec,
+                    'language', sub.language
+                )
+            ) FILTER (WHERE sub.id IS NOT NULL), '[]'
         ) as subtitles
     FROM movie_base m
     LEFT JOIN video_streams vs ON vs.movie_id = m.id
@@ -146,12 +159,15 @@ movie_streams AS (
 ),
 movie_chapters AS (
     SELECT COALESCE(
-        json_agg(DISTINCT jsonb_build_object(
-            'id', ch.id,
-            'title', ch.title,
-            'start_time_ms', ch.start_time_ms,
-            'thumb', ch.thumb
-        ) ORDER BY ch.start_time_ms) FILTER (WHERE ch.id IS NOT NULL), '[]'
+        json_agg(
+            jsonb_build_object(
+                'id', ch.id,
+                'title', ch.title,
+                'start_time_ms', ch.start_time_ms,
+                'thumb', ch.thumb
+            )
+            ORDER BY ch.start_time_ms
+        ) FILTER (WHERE ch.id IS NOT NULL), '[]'
     ) as chapters
     FROM movie_base m
     LEFT JOIN chapters ch ON ch.movie_id = m.id
