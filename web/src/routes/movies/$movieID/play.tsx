de0061622d8f6dca96a5a8e8d7cb275d547dbadd
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2025-02-21 22:29:11 -0400

    multi audio channels now working with hls streaming

--- web/src/routes/movies/$movieID/play.tsx
+++ web/src/routes/movies/$movieID/play.tsx
@@ -1,53 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
 import canPlayNativeHls from "@/utils/canPlayNativeHls";
 import HlsPlayer from "@/components/HlsPlayer";
-import type { MovieHlsOpts } from "@/types/Transcode";
+
+type PlayParams = {
+  title: string;
+  pid: string;
+};
 
 export const Route = createFileRoute("/movies/$movieID/play")({
-  validateSearch: (search: Record<string, unknown>): MovieHlsOpts => ({
+  validateSearch: (search: Record<string, unknown>): PlayParams => ({
     title: String(search.title ?? "unknown"),
-    audio_codec: String(search.audio_codec),
-    audio_bit_rate: Number(search.audio_bit_rate),
-    audio_channels: Number(search.audio_channels),
-    audio_stream_index: Number(search.audio_stream_index),
-    video_codec: String(search.video_codec),
-    video_stream_index: Number(search.video_stream_index),
-    video_bit_rate: Number(search.video_bit_rate),
-    video_height: Number(search.video_height),
-    video_profile: String(search.video_profile),
-    preset: String(search.preset),
+    pid: String(search.pid ?? "unknown"),
   }),
   loaderDeps: ({ search }) => search,
-  loader: async ({ deps, params }) => {
-    const res = await fetch(
-      `/api/v1/ffmpeg/movie/create-hls/${params.movieID}`,
-      {
-        method: "post",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(deps),
-      }
-    );
-
-    const data = await res.json();
-
-    if (data.error) {
-      throw data.error;
-    }
-
-    return {
-      pid: data?.pid,
-      movieID: params.movieID,
-      title: deps.title,
-    };
-  },
   component: PlayMoviePage,
 });
 
 function PlayMoviePage() {
-  const { movieID, title, pid } = Route.useLoaderData();
+  const { movieID } = Route.useParams();
+  const { pid, title } = Route.useLoaderDeps();
 
   console.log(pid);
 
