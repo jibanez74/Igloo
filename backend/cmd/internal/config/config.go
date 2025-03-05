a1e5bd41717475f5e7ffa7cc982c90fbc0fc9997
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2025-03-05 11:17:14 -0400

    working on proper creation of dirs

--- backend/cmd/internal/config/config.go
+++ backend/cmd/internal/config/config.go
@@ -31,57 +31,55 @@ func New() (*config, error) {
 	sharePath := filepath.Join(homeDir, ".local", "share", "igloo")
 
 	_, err = os.Stat(configPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			_, err = os.Create(configPath)
-			if err != nil {
-				return nil, fmt.Errorf("failed to create config file: %w", err)
-			}
+	if os.IsNotExist(err) {
+		configDir := filepath.Dir(configPath)
 
-			port, err := strconv.Atoi(os.Getenv("PORT"))
-			if err != nil {
-				port = 8080
-			}
-			s.Port = int32(port)
+		err = os.MkdirAll(configDir, 0755)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create config directory: %w", err)
+		}
 
-			debug, err := strconv.ParseBool(os.Getenv("DEBUG"))
-			if err != nil {
-				debug = true
-			}
-			s.Debug = debug
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			port = 8080
+		}
+		s.Port = int32(port)
 
-			downloadImages, err := strconv.ParseBool(os.Getenv("DOWNLOAD_IMAGES"))
-			if err != nil {
-				downloadImages = false
-			}
-			s.DownloadImages = downloadImages
+		debug, err := strconv.ParseBool(os.Getenv("DEBUG"))
+		if err != nil {
+			debug = true
+		}
+		s.Debug = debug
 
-			err = cfg.createDirs(sharePath)
-			if err != nil {
-				return nil, fmt.Errorf("failed to create dirs: %w", err)
-			}
+		downloadImages, err := strconv.ParseBool(os.Getenv("DOWNLOAD_IMAGES"))
+		if err != nil {
+			downloadImages = false
+		}
+		s.DownloadImages = downloadImages
 
-			s.FfmpegPath = os.Getenv("FFMPEG_PATH")
-			s.HardwareAcceleration = os.Getenv("HARDWARE_ACCELERATION")
-			s.FfprobePath = os.Getenv("FFPROBE_PATH")
-			s.TmdbApiKey = os.Getenv("TMDB_API_KEY")
-			s.JellyfinToken = os.Getenv("JELLYFIN_TOKEN")
+		err = cfg.createDirs(sharePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create dirs: %w", err)
+		}
 
-			cfg.CreateSettings = true
+		s.FfmpegPath = os.Getenv("FFMPEG_PATH")
+		s.HardwareAcceleration = os.Getenv("HARDWARE_ACCELERATION")
+		s.FfprobePath = os.Getenv("FFPROBE_PATH")
+		s.TmdbApiKey = os.Getenv("TMDB_API_KEY")
+		s.JellyfinToken = os.Getenv("JELLYFIN_TOKEN")
 
-			jsonData, err := json.MarshalIndent(s, "", "  ")
-			if err != nil {
-				return nil, fmt.Errorf("failed to marshal settings: %w", err)
-			}
+		cfg.CreateSettings = true
 
-			err = os.WriteFile(configPath, jsonData, 0644)
-			if err != nil {
-				return nil, fmt.Errorf("failed to write config file: %w", err)
-			}
-		} else {
-			return nil, fmt.Errorf("failed to check config file: %w", err)
+		jsonData, err := json.MarshalIndent(s, "", "  ")
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal settings: %w", err)
+		}
+
+		err = os.WriteFile(configPath, jsonData, 0644)
+		if err != nil {
+			return nil, fmt.Errorf("failed to write config file: %w", err)
 		}
 	}
-
+ 
 	return &cfg, nil
 }
