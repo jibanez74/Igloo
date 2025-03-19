b24e90f1b3fe76f0d574d404870f2fa97d172912
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2025-03-19 14:04:41 -0400

    added a new client build

---
+++ backend/cmd/client/assets/index-CPLCWxvn.js
@@ -0,0 +1,1 @@
+import{t}from"./index-MKo_I7om.js";var o=t('<div>Hello "/tvshows/"!');const p=function(){return o()};export{p as component};
---
+++ backend/cmd/client/assets/index-D5pU8S_k.js
@@ -0,0 +1,1 @@
+import{t}from"./index-MKo_I7om.js";var o=t('<div>Hello "/music/"!');const m=function(){return o()};export{m as component};
