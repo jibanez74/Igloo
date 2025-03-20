f11cbbda82d13fd5192191b5ef4477d7769efc53
Jose Ibañez <jose.ibanez@blindpenguincoder.com>
2025-03-19 20:11:38 -0400

    added new web build

---
+++ backend/cmd/client/assets/index-Cz0mjRsu.js
@@ -0,0 +1,1 @@
+import{t}from"./index-DIyVjOz_.js";var o=t('<div>Hello "/tvshows/"!');const p=function(){return o()};export{p as component};
---
+++ backend/cmd/client/assets/index-NcQCI8ub.js
@@ -0,0 +1,1 @@
+import{t}from"./index-DIyVjOz_.js";var o=t('<div>Hello "/music/"!');const m=function(){return o()};export{m as component};
